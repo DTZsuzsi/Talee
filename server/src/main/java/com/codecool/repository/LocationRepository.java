a609867e5a1bcc534fbae3ddd2488953064a9c0e
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-11-12 13:38:45 +0100

    implementing LocationRepository interface

--- server/src/main/java/com/codecool/repository/LocationRepository.java
+++ server/src/main/java/com/codecool/repository/LocationRepository.java
@@ -1,4 +1,8 @@
 package com.codecool.repository;
 
-public interface LocationRepository {
+import com.codecool.model.location.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LocationRepository extends JpaRepository<Location, Integer> {
+  Location getLocationById(int id);
 }
