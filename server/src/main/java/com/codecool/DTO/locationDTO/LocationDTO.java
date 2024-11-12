346dbf126191635cd7650a3357a1e1c2e2292e29
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-11-12 13:53:16 +0100

    introducing new parameter (User adminUser) into DTOs

--- server/src/main/java/com/codecool/DTO/locationDTO/LocationDTO.java
+++ server/src/main/java/com/codecool/DTO/locationDTO/LocationDTO.java
@@ -1,5 +1,7 @@
 package com.codecool.DTO.locationDTO;
 
+import com.codecool.model.User;
+
 //TODO use ContactInfoDTO, OpeningHoursDTO and EventDTO instead?
 public record LocationDTO(int id, String name, String address, String phone, String email, String description, User adminUser) {
 }
