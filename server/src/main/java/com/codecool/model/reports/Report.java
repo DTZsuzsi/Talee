4d04194a869a310679b763c441cef14ab4e75494
mmarci96 <sarosdimarci@gmail.com>
2024-11-13 16:26:53 +0100

    created the report entity

---
+++ server/src/main/java/com/codecool/model/reports/Report.java
@@ -0,0 +1,41 @@
+package com.codecool.model.reports;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@Table(name = "reports")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Report {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String description;
+    private ReportType reportType;
+    private boolean issued;
+    private boolean solved;
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+
+}
