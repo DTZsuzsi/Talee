135271b4c0f61ef7ff21f47d26b2926108d964fb
mmarci96 <sarosdimarci@gmail.com>
2024-11-13 17:31:46 +0100

    extended datasql with some fake data

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -22,3 +22,31 @@ VALUES ('2024-11-02', 'Állatkertek éjszakája', 'Éjjel is jöhettek', 1, 'Mar
 
 INSERT INTO tag_category(name, color) VALUES('company type', 'green'), ('sporty', 'blue'), ('culture', 'black'),('romantic', 'red'), ('family', 'yellow'),('prices', 'grey'),('gastronomy', 'brown'), ('other', 'purple');
 INSERT INTO tag(name, tag_category_id) VALUES ('big',1), ('romantic',4),('family friendly',5), ('hiking',2);
+
+-- Reports for issues and content for testing purposes c:
+
+INSERT INTO reports (title, description, report_type, issued, solved, created_at, updated_at)
+VALUES
+    ('Missing Event Details',
+     'The event "Állatkertek éjszakája" does not have a full description or schedule. Please update with more information.',
+     'CONTENT',
+     true,
+     false,
+     '2024-10-15 10:30:00',
+     '2024-10-15 10:30:00'),
+
+    ('Error in Location Address',
+     'The address for Margitsziget is incorrect on the event page. It displays "1146 Budapest" instead of "1138 Budapest".',
+     'BUG',
+     true,
+     true,
+     '2024-09-20 14:00:00',
+     '2024-10-01 09:00:00'),
+
+    ('New Activity Suggestion',
+     'Suggest adding a family picnic event on Margitsziget with food vendors and outdoor games to make it more family-friendly.',
+     'CONTENT',
+     false,
+     false,
+     '2024-11-05 16:45:00',
+     '2024-11-05 16:45:00');
