4166dd8d55d5f1aeff0d78d0f48402d5f35a509c
mmarci96 <sarosdimarci@gmail.com>
2024-11-13 17:43:21 +0100

    post and get requests added but i suck at exeption handling help pls

--- server/src/main/java/com/codecool/controller/ReportController.java
+++ server/src/main/java/com/codecool/controller/ReportController.java
@@ -1,13 +1,12 @@
 package com.codecool.controller;// ReportController.java
 
 import com.codecool.DTO.reportDTO.ReportDTO;
+import com.codecool.DTO.reportDTO.ReportReqDTO;
+import com.codecool.DTO.reportDTO.ReportResDTO;
 import com.codecool.model.reports.ReportType;
 import com.codecool.service.ReportService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +29,15 @@ public class ReportController {
             @RequestParam(defaultValue = "createdAt") String sortBy,
             @RequestParam(defaultValue = "asc") String order) {
         return reportService.getAllReports(reportType, issued, solved, sortBy, order);
+    }
+
+    @GetMapping("/{id}")
+    public ReportDTO getReportById(@PathVariable long id) {
+        return reportService.getReportById(id);
+    }
+
+    @PostMapping
+    public ReportResDTO addReport(@RequestBody ReportReqDTO reportDTO) {
+        return reportService.addReport(reportDTO);
     }
 }
