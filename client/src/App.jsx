e496bc08734ca9f5caa5426ce755f6cab6e18b69
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-11-13 14:38:20 +0100

    managing front-end for location details page and including delete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,12 +4,7 @@ import Home from "./features/main/Home.jsx";
 import Welcome from "./features/welcome/Welcome.jsx";
 import Layout from "./features/main/components/organisms/Layout.jsx";
 
-import EventDetailPage from './features/events/EventDetailPage.jsx';
-import NewEventForm from './features/events/NewEventForm.jsx';
 
-import ModifyEventForm from './features/events/ModifyEventForm.jsx';
-
-import NewLocationForm from "./features/locations/NewLocationForm.jsx";
 import LocationDetailPage from "./features/locations/LocationDetailPage.jsx";
 import AllTagsPage from './features/tag/AllTagsPage.jsx';
 import AllTagCategoriesPage from './features/tag/AllTagCategoriesPage.jsx';
