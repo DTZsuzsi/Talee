a5481694204d9eba6a2a5a6392efda5de0cdb2fd
mmarci96 <sarosdimarci@gmail.com>
2024-11-14 14:25:32 +0100

    added a server error component to display unsuccesfull post requests

--- client/src/features/main/components/molecules/PageNotFound.jsx
+++ client/src/features/main/components/molecules/PageNotFound.jsx
@@ -1,9 +1,10 @@
+
 const PageNotFound = () => {
-  return (
-    <div className="flex justify-center items-center">
-      <h1 className="text-3xl font-bold" >Page not found</h1>
-    </div>
-  )
+	return (
+		<div className='flex justify-center items-center'>
+			<h1 className='text-3xl font-bold'>Page not found</h1>
+		</div>
+	);
 };
 
-export default PageNotFound
+export default PageNotFound;
