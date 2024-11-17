2e6dee494382d267fcbb504aad84abcf25fda43d
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-11-17 16:20:42 +0100

    implementing openinghoursnotfoundexception

--- server/src/main/java/com/codecool/controller/GlobalExceptionHandler.java
+++ server/src/main/java/com/codecool/controller/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package com.codecool.controller;
 
 import com.codecool.exceptions.LocationNotFoundException;
+import com.codecool.exceptions.OpeningHoursNotFoundException;
 import com.codecool.exceptions.UserNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -21,5 +22,9 @@ public class GlobalExceptionHandler {
     public String handleLocationNotFoundException(LocationNotFoundException ex) {
         return ex.getMessage();
     }
+
+    @ExceptionHandler(OpeningHoursNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleOpeningHoursNotFoundException(OpeningHoursNotFoundException ex) {return ex.getMessage();}
 }
 
--- server/src/main/java/com/codecool/exceptions/OpeningHoursNotFoundException.java
+++ server/src/main/java/com/codecool/exceptions/OpeningHoursNotFoundException.java
@@ -1,7 +1,7 @@
 package com.codecool.exceptions;
 
 public class OpeningHoursNotFoundException extends RuntimeException {
-  public OpeningHoursNotFoundException(String message) {
-    super(message);
+  public OpeningHoursNotFoundException(long openingHoursId) {
+    super("OpeningHours with id " + openingHoursId + " not found");
   }
 }
