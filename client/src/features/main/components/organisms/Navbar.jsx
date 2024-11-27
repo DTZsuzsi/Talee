a67ff0f0035cc8c98ddfc3a322b334e75c7ec8b9
matet2001 <matet2001@gmail.com>
2024-11-27 19:16:14 +0100

    Add route to navbar

--- client/src/features/main/components/organisms/Navbar.jsx
+++ client/src/features/main/components/organisms/Navbar.jsx
@@ -8,8 +8,8 @@ import {Link} from "react-router-dom";
 import {CgProfile} from "react-icons/cg";
 
 const navigation = [
-    // { name: 'Dashboard', href: '#', current: true },
-    // { name: 'Team', href: '#', current: false },
+    // { name: 'Add Event', href: '#', current: true },
+    { name: 'Add Location', href: '/locations/new', current: false },
     // { name: 'Projects', href: '#', current: false },
     // { name: 'Calendar', href: '#', current: false },
 ]
@@ -69,7 +69,7 @@ export default function Navbar() {
                                         href={item.href}
                                         aria-current={item.current ? 'page' : undefined}
                                         className={classNames(
-                                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                                            item.current ? 'bg-gray-900 text-white' : 'hover:text-white',
                                             'rounded-md px-3 py-2 text-sm font-medium',
                                         )}
                                     >
