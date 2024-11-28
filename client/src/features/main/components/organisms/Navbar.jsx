9216c9f0c6a4385e28c18bac891ed4eb1c256c6d
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-11-28 22:25:15 +0100

    fixing navbar after merge hiccup

--- client/src/features/main/components/organisms/Navbar.jsx
+++ client/src/features/main/components/organisms/Navbar.jsx
@@ -1,11 +1,11 @@
 import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-  Menu,
-  MenuButton,
-  MenuItem,
-  MenuItems,
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
 } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import TaleeLogo from "../atoms/TaleeLogo.jsx";
@@ -16,35 +16,35 @@ import { Link } from "react-router-dom";
 import { CgProfile } from "react-icons/cg";
 
 const navigation = [
-  // { name: 'Add Event', href: '#', current: true },
-  { name: "Add Location", href: "/locations/new", current: false },
-  // { name: 'Projects', href: '#', current: false },
-  // { name: 'Calendar', href: '#', current: false },
+    // { name: 'Add Event', href: '#', current: true },
+    { name: "Add Location", href: "/locations/new", current: false },
+    // { name: 'Projects', href: '#', current: false },
+    // { name: 'Calendar', href: '#', current: false },
 ];
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
+    return classes.filter(Boolean).join(" ");
 }
 
 export default function Navbar() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const username = localStorage.getItem("userName");
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const username = localStorage.getItem("userName");
 
-  const checkLogin = () => {
-    return localStorage.getItem("jwtToken");
-  };
+    const checkLogin = () => {
+        return localStorage.getItem("jwtToken");
+    };
 
-  const handleLogout = () => {
-    localStorage.removeItem("jwtToken");
-    localStorage.removeItem("userName");
-    setIsLoggedIn(false);
-  };
+    const handleLogout = () => {
+        localStorage.removeItem("jwtToken");
+        localStorage.removeItem("userName");
+        setIsLoggedIn(false);
+    };
 
-  useEffect(() => {
-    setIsLoggedIn(checkLogin());
-  }, []);
+    useEffect(() => {
+        setIsLoggedIn(checkLogin());
+    }, []);
 
-  return (
+    return (
     <Disclosure as="nav" className="">
       <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div className="relative flex h-16 items-center justify-between">
@@ -144,7 +144,7 @@ export default function Navbar() {
                     </div>
                 </div>
             </div>
-          </div>
+          
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
@@ -199,29 +199,29 @@ export default function Navbar() {
               </MenuItems>
             </Menu>
           </div>
-        </div>
-      </div>
+        
+     
 
-      <DisclosurePanel className="sm:hidden">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          {navigation.map((item) => (
-            <DisclosureButton
-              key={item.name}
-              as="a"
-              href={item.href}
-              aria-current={item.current ? "page" : undefined}
-              className={classNames(
-                item.current
-                  ? "bg-gray-900 text-white"
-                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                "block rounded-md px-3 py-2 text-base font-medium",
-              )}
-            >
-              {item.name}
-            </DisclosureButton>
-          ))}
-        </div>
-      </DisclosurePanel>
+        <DisclosurePanel className="sm:hidden">
+            <div className="space-y-1 px-2 pb-3 pt-2">
+                {navigation.map((item) => (
+                    <DisclosureButton
+                        key={item.name}
+                        as="a"
+                        href={item.href}
+                        aria-current={item.current ? "page" : undefined}
+                        className={classNames(
+                            item.current
+                                ? "bg-gray-900 text-white"
+                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "block rounded-md px-3 py-2 text-base font-medium",
+                        )}
+                    >
+                        {item.name}
+                    </DisclosureButton>
+                ))}
+            </div>
+        </DisclosurePanel>
     </Disclosure>
   );
 }
