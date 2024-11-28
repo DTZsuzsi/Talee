3c498d3019e0e5e0076bc8e479fc8ddcd52ba3c5
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-11-28 15:50:07 +0100

    assigning location owner role to initial user in data.sql

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -5,7 +5,7 @@ INSERT INTO talee_user(username, password)
 VALUES ('matet', '$2a$10$0F2KZTUFjkzR9.BOrEWZQ.V30PbhYHi1BhktPgDZJh0n9YJorH/A6'); --, ('zsuzsi', '$2a$10$0F2KZTUFjkzR9.BOrEWZQ.V30PbhYHi1BhktPgDZJh0n9YJorH/A6', 'initialEmail2@email.com'), ('mariann', '$2a$10$0F2KZTUFjkzR9.BOrEWZQ.V30PbhYHi1BhktPgDZJh0n9YJorH/A6', 'initialEmail3@email.com');
 
 INSERT INTO talee_user_roles(roles_id, user_entity_id)
-VALUES (1, 1);
+VALUES (1, 1), (3, 1);
 
 INSERT INTO location (name, admin_user_id, address, phone, email, website, facebook, instagram, description)
 VALUES
