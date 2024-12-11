724e66911b2263b0756748deb9b331cd8881f9cf
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2024-12-11 10:54:22 +0100

    taglist added

--- client/src/features/locations/UpdateLocationForm.jsx
+++ client/src/features/locations/UpdateLocationForm.jsx
@@ -4,13 +4,13 @@ import { useNavigate, useParams } from 'react-router-dom';
 import TaleeButton from '../main/components/atoms/TaleeButton.jsx';
 import Loading from '../main/components/atoms/Loading';
 import ServerError from '../main/components/atoms/ServerError';
-import TagOptions from '../tag/components/TagOptions.jsx';
-import TagCard from '../tag/components/TagCard.jsx';
+
 import LocationForm from './LocationForm.jsx';
 import { useFetchTags } from '../main/components/hooks/useFetchTags.jsx';
-import { useTagHandlers } from '../main/tagHandling/useTagHandlers.jsx';
+import { useTagHandlers } from '../main/tagHandling/useTagHandlers.jsx'
 import useOpeningHours from './hooks/useOpeningHours.jsx';
 import axios from 'axios';
+import TagListModify from '../main/components/molecules/TagListModify.jsx';
 function UpdateLocationForm() {
   const { locationId } = useParams();
   const [location, setLocation] = useState(null);
@@ -75,7 +75,7 @@ function UpdateLocationForm() {
 
           <LocationForm location={location} setLocation={setLocation} onHoursChange={handleOpeningHoursChange} />
 
-          <div className='w-full my-5'>
+          {/* <div className='w-full my-5'>
             <TagOptions onChange={handleNewTag} />
             <ul className='grid grid-cols-2 md:grid-cols-4 gap-4'>
               {location.tags?.map((tag) => (
@@ -84,7 +84,9 @@ function UpdateLocationForm() {
                 </li>
               ))}
             </ul>
-          </div>
+          </div> */}
+
+          <TagListModify location={location} setLocation={setLocation} tags={tags} />
 
           <div className='w-full flex justify-center mt-6'>
             {loading ? (
