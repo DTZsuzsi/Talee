38b809be0deaa38e515d2d432f54ac5137b41d73
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2024-12-11 15:14:18 +0100

    eventdetail done

--- client/src/features/events/ModifyEventForm.jsx
+++ client/src/features/events/ModifyEventForm.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import InputField from "../main/components/atoms/InputField";
