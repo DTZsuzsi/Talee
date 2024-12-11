3d8259db70e07ec673610fd09c2b12c3429c90aa
matet2001 <matet2001@gmail.com>
2024-12-11 10:20:43 +0100

    Format location detailed page

--- client/src/features/locations/LocationDetailPage.jsx
+++ client/src/features/locations/LocationDetailPage.jsx
@@ -1,12 +1,6 @@
-import { useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
-import { HiMiniPencilSquare } from "react-icons/hi2";
-import { MdDeleteForever } from "react-icons/md";
 import ServerError from "../main/components/atoms/ServerError";
 import Loading from "../main/components/atoms/Loading";
-import TagCard from "../tag/components/TagCard";
-import BiggerOnHover from "../main/components/atoms/BiggerOnHover";
-import { APIProvider, Map, Marker } from "@vis.gl/react-google-maps";
 import TaleeButton from "../main/components/atoms/TaleeButton.jsx";
 import MapDisplay from "../maps/MapDisplay.jsx";
 import { useFetchLocationData } from "./hooks/useFetchLocationData.jsx";
@@ -18,8 +12,8 @@ import ActionButtons from "../main/components/molecules/ActionButtons.jsx";
 function LocationDetailPage() {
   const { locationId } = useParams();
   const navigate = useNavigate();
-  const {location, events, error, loading, owner, user}=useFetchLocationData(locationId);
-
+  const { location, events, error, loading, owner, user } =
+    useFetchLocationData(locationId);
 
   async function deleteLocation() {
     const response = await fetch(`/api/locations/${locationId}`, {
@@ -44,25 +38,25 @@ function LocationDetailPage() {
   return (
     <div className="flex flex-col items-center justify-center">
       <div className="p-6 my-10 border rounded-lg shadow-md bg-light-secondaryBg dark:bg-dark-secondaryBg border-light-border dark:border-dark-border w-full max-w-4xl">
-        <LocationInfo location={location}/>
+        <LocationInfo location={location} />
 
         {location.latitude && (
-          <MapDisplay lat={location.latitude} lng={location.longitude}/>
+          <MapDisplay lat={location.latitude} lng={location.longitude} />
         )}
         {events.length > 0 && (
           <div>
-           <EventList events={events}/>
+            <EventList events={events} />
           </div>
         )}
 
         {location?.tags?.length > 0 && (
           <div className="mt-5">
-           <TagList tags={location.tags}/>
+            <TagList tags={location.tags} />
           </div>
         )}
         {owner === user && (
           <div className="flex justify-end gap-4 mt-6">
-            <ActionButtons id={locationId} partName={"location"}/>
+            <ActionButtons id={locationId} partName={"location"} />
 
             <Link to={`/events/new/${locationId}`}>
               <TaleeButton> Create event</TaleeButton>
