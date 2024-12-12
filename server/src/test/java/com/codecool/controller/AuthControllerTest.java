920f75065d50c784d8a78d1986bd84da89bb9666
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-12-12 19:14:18 +0100

    implementing register integration test happy case

---
+++ server/src/test/java/com/codecool/controller/AuthControllerTest.java
@@ -0,0 +1,46 @@
+package com.codecool.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+class AuthControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  private String userJson;
+
+  @BeforeEach
+  void setUp() {
+    userJson = """
+            {
+            "username": "testUser",
+            "password": "password"
+            }
+            """;
+  }
+
+  @Test
+  void whenUsernameAndPasswordSent_thenRegisterSuccessfully() throws Exception {
+    mockMvc.perform(post("/api/auth/register")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(userJson))
+            .andExpect(status().isCreated());
+  }
+
+  @Test
+  void login() {
+  }
+}
