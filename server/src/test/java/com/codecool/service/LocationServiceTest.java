8d3212c594db7fd4aec1616d23b97aef65964dc1
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-12-12 10:45:03 +0100

    adding unit test skeleton for locationservice

---
+++ server/src/test/java/com/codecool/service/LocationServiceTest.java
@@ -0,0 +1,36 @@
+package com.codecool.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationServiceTest {
+
+  @Test
+  void getAllLocations() {
+  }
+
+  @Test
+  void getLocationById() {
+  }
+
+  @Test
+  void addLocation() {
+  }
+
+  @Test
+  void deleteLocation() {
+  }
+
+  @Test
+  void addTagToLocation() {
+  }
+
+  @Test
+  void deleteTagFromLocation() {
+  }
+
+  @Test
+  void updateLocation() {
+  }
+}
