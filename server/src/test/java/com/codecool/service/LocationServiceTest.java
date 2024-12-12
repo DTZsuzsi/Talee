74d8067292c34ac2886ee1f324a9c2458d87963b
Marianna_Maria_Molnar <152993474+MariannaMolnar@users.noreply.github.com>
2024-12-12 11:56:19 +0100

    renaming 2 scenarios for getAllLocations unit tests

--- server/src/test/java/com/codecool/service/LocationServiceTest.java
+++ server/src/test/java/com/codecool/service/LocationServiceTest.java
@@ -1,13 +1,53 @@
 package com.codecool.service;
 
+import com.codecool.mapper.LocationMapper;
+import com.codecool.mapper.TagMapper;
+import com.codecool.mapper.UserMapper;
+import com.codecool.repository.LocationRepository;
+import com.codecool.repository.RoleRepository;
+import com.codecool.repository.TagCategoryRepository;
+import com.codecool.repository.UserRepository;
+import com.codecool.security.JWTUtils;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+@ExtendWith(MockitoExtension.class)
 class LocationServiceTest {
+  @Mock
+  private LocationRepository locationRepository;
+  @Mock
+  private OpeningHoursService openingHoursService;
+  @Mock
+  private TagCategoryRepository tagCategoryRepository;
+  @Mock
+  private UserService userService;
+  @Mock
+  private RoleRepository roleRepository;
+  @Mock
+  private LocationMapper locationMapper;
+  @Mock
+  private UserMapper userMapper;
+  @Mock
+  private TagMapper tagMapper;
+  @Mock
+  private JWTUtils jwtUtils;
+  @Mock
+  private UserRepository userRepository;
+
+  @InjectMocks
+  private LocationService locationService;
 
   @Test
-  void getAllLocations() {
+  void whenLocationsGetAllLocations_returnsListOfLocationDTOs() {
+
+  }
+
+  @Test
+  void whenNoLocationsGetAllLocations_returnsEmptyList() {
   }
 
   @Test
