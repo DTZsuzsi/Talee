c342fdc5b001bdc3edd82c8b34e6f90fc0af8964
Ditroi_Toth_Zsuzsa <157112767+DTZsuzsi@users.noreply.github.com>
2025-01-30 08:43:01 +0100

    newlocationdto corrected

--- server/src/main/java/com/codecool/talee/DTO/location/NewLocationDTO.java
+++ server/src/main/java/com/codecool/talee/DTO/location/NewLocationDTO.java
@@ -7,5 +7,5 @@ import com.fasterxml.jackson.annotation.JsonManagedReference;
 import java.util.List;
 
 public record NewLocationDTO(String name, String address, String phone, String email, String website, String facebook,
-                             String instagram, String description, @JsonManagedReference UserDTO adminUser, List<NewOpeningHoursWithoutLocationDTO> openingHours, double latitude, double longitude) {
+                             String instagram, String description,  UserDTO adminUser, List<NewOpeningHoursWithoutLocationDTO> openingHours, double latitude, double longitude) {
 }
